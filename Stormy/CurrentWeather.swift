2342e35a13308886e901fc27bf139b924a1eac53
enrique7mc <enrique7mc@gmail.com>
2015-07-17 09:39:47 -0500

    Adds weather model

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,23 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by admin on 17/07/15.
+//  Copyright (c) 2015 enrique7mc. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Int
+    let humidity: Int
+    let precipProbability: Int
+    let summary: String
+    
+    init(weatherDictionary: [String: AnyObject]) {
+        temperature = weatherDictionary["temperature"] as! Int
+        humidity = weatherDictionary["humidity"] as! Int
+        precipProbability = weatherDictionary["precipChance"] as! Int
+        summary = weatherDictionary["summary"] as! String
+    }
+}
