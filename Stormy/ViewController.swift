e6fc7f8b11a3d131020598339bda7f9ddf45808c
enrique7mc <enrique7mc@gmail.com>
2015-07-17 10:16:04 -0500

    Reads weather data from file plist

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
+            let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
+            let currentWeatherDictionary = weatherDictionary["currently"] as? [String: AnyObject]{
+            
+            let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
+        }
+        
+        if let plistPath = NSBundle.mainBundle().pathForResource("CrazyInformation", ofType: "plist"),
+            let weatherDictionary = NSDictionary(contentsOfFile: plistPath){
+        }
     }
 
     override func didReceiveMemoryWarning() {
