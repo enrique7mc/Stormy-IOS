ed45704af6f5c6f0c71dc377811a08e1f7cf34df
enrique7mc <enrique7mc@gmail.com>
2015-08-03 10:15:57 -0500

    Updates UI

--- Stormy/WeeklyTableTableViewController.swift
+++ Stormy/WeeklyTableTableViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class WeeklyTableTableViewController: UITableViewController {
 
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentTemperatureRangeLabel: UILabel!
+    
+    let coordinate: (lat: Double, long: Double) = (19.42847,-99.12766)
+    private let forecastKey = "807f790501f3841dd94c54f8ef1e7ff2"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureView()
+        retrieveWeatherForecast()
+    }
+    
+    func configureView() {
+        // set tableview background view
         tableView.backgroundView = BackgroundView()
+        
+        // change font and size
+        if let navBarFont = UIFont(name: "HelveticaNeue-Thin", size: 20.0) {
+            let navBarAttributesDictionary: [NSObject: AnyObject]? = [
+                NSForegroundColorAttributeName: UIColor.whiteColor(),
+                NSFontAttributeName: navBarFont
+            ]
+            
+            navigationController?.navigationBar.titleTextAttributes = navBarAttributesDictionary
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,59 +60,28 @@ class WeeklyTableTableViewController: UITableViewController {
         return 0
     }
 
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
-        return cell
+    // MARK: - Weather Fetching
+    
+    func retrieveWeatherForecast() {
+        let forecastService = ForecastService(APIKey: forecastKey)
+        forecastService.getForecast(coordinate.lat, long: coordinate.long) {
+            (let currently) in
+            if let currentWeather = currently {
+                dispatch_async(dispatch_get_main_queue()) {
+                    if let temperature = currentWeather.temperature {
+                        self.currentTemperatureLabel?.text = "\(temperature)º"
+                    }
+                    
+                    if let precipitation = currentWeather.precipProbability {
+                        self.currentPrecipitationLabel?.text = "Rain: \(precipitation)%"
+                    }
+                    
+                    if let icon = currentWeather.icon {
+                        self.currentWeatherIcon?.image = icon
+                    }
+                }
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
